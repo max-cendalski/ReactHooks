5f5bfde6e9b77ea253a1d08490ddd2ba99df9004
Korneliu <olbiton@gmail.com>
2021-09-23 17:28:18 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Andrew Mead
         </a>
       </header>
     </div>
